5d62989ab6858c7c79a0885d0457867f5c43b836
Vu Vo Thanh <vu.voth@gmail.com>
2019-01-13 22:29:35 +0700

    add parquet format

---
+++ src/main/java/AvroSchema.java
@@ -0,0 +1,107 @@
+/*
+ * File Name: AvroSchema.java
+ *
+ * File Description:
+ *
+ * Create schema for Avro library. The schema description for one Petri net(the state of Pn).
+ * Link https://avro.apache.org/docs/1.8.1/spec.html
+ *
+ * Copyright (c) 2019 - Ferdon Vietnam Limited
+ */
+
+import org.apache.commons.io.FileUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class AvroSchema {
+
+    JSONObject obj;
+
+    public JSONObject readJson(String filename) {
+        JSONObject object;
+        try {
+            File file = new File(filename);
+            String content = FileUtils.readFileToString(file, "UTF-8");
+            object = new JSONObject(content);
+            return object;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String field(String name, String type) {
+        String ans = "";
+        ans = ans + "\t{\"name\" : \"" + name + "\" ,";
+        if (type.equals("unit") == true)
+            type = "int";
+        if (type.equals("bool"))
+            type = "boolean";
+        ans = ans + " \"type\" : [\"" + type + "\", \"null\"] }\n";
+        return ans;
+    }
+
+    public String fieldRecord(String name, String fields) {
+        String ans = "";
+        String[] sp = fields.split("\\*");
+        ans = ans + "{\n \"name\" : \"" + name + "\" ,\n";
+        ans = ans + " \"type\"  : \"record\" ,\n";
+        ans = ans + " \"fields\" : [\n";
+        int k = 0;
+        String tmp = "";
+        for (String s : sp) {
+            if (k > 0)
+                ans = ans + ",";
+            tmp = s.toLowerCase();
+            ans = ans + field(tmp + "_" + k, tmp);
+            k++;
+        }
+        ans = ans + "]}";
+        return ans;
+    }
+
+    public String Record(String name, String fields) {
+        String ans = "";
+        ans = ans + "{\"name\" : \"" + name + "\", \"type\": {\"type\":\"array\", \"items\":" + fieldRecord("Data" + name, fields) + "}}";
+        return ans;
+    }
+
+    public String createAvroSchema(String filename) {
+        String s = "";
+        s = s + "{\n \"name\" : \"PetriNet\" ,\n";
+        s = s + " \"type\"  : \"record\" ,\n";
+        s = s + " \"fields\" : [\n";
+
+        //System.out.println(s);
+        obj = readJson(filename);
+        JSONArray objArray = obj.getJSONArray("color");
+        int k = 0;
+        for (Object color : objArray) {
+            if (k > 0)
+                s = s + ",\n";
+            s = s + Record("P" + k, (String) color);
+            k++;
+        }
+        s = s + ",{\"name\":\"id\", \"type\":\"int\"}\n";
+        s = s + "]}";
+        return s;
+    }
+
+    public void createSchemaFile(String inputfile, String outputfile) {
+        String s = createAvroSchema(inputfile);
+        try {
+            FileOutputStream outputStream = new FileOutputStream(outputfile);
+            byte[] strToBytes = s.getBytes();
+            outputStream.write(strToBytes);
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
