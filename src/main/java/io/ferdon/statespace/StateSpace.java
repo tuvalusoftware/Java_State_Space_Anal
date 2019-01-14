1f4186cb5df68aee96e32312c7557a4dc49dc49b
Dang Duy Nam <dangduynam95@gmail.com>
2019-01-14 14:51:54 +0700

    Merge parquet feature branch and clean up code

--- src/main/java/io/ferdon/statespace/StateSpace.java
+++ src/main/java/io/ferdon/statespace/StateSpace.java
@@ -1,9 +1,20 @@
 package io.ferdon.statespace;
 
 import com.google.common.collect.Multiset;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericArray;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.parquet.avro.AvroParquetWriter;
+import org.apache.parquet.hadoop.ParquetWriter;
+import org.apache.parquet.hadoop.metadata.CompressionCodecName;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -115,12 +126,150 @@ public class StateSpace {
         }
         return result;
     }
-    
-
-
 
     void print(String s){
         System.out.println(s);
+    }
+
+    public static String getType(String ty) {
+        String s[] = ty.split("_");
+        return s[0];
+    }
+
+    public static Object getValue(String input, String type) {
+        type = getType(type);
+        if (type.equals("int")) {
+            return Integer.parseInt(input);
+        }
+        if (type.equals("string")) {
+            return input;
+        }
+        if (type.equals("double")) {
+            return Double.parseDouble(input);
+        }
+        if (type.equals("bool")) {
+            return Boolean.parseBoolean(input);
+        }
+        //type is unit
+        return null;
+    }
+
+
+    ParquetWriter<GenericRecord> parquetWriter(String outputFile, Schema schema) {
+        ParquetWriter<GenericRecord> writer = null;
+        try {
+            writer = AvroParquetWriter.
+                    <GenericRecord>builder(new Path(outputFile))
+                    .withRowGroupSize(ParquetWriter.DEFAULT_BLOCK_SIZE)
+                    .withPageSize(ParquetWriter.DEFAULT_PAGE_SIZE)
+                    .withSchema(schema)
+                    .withConf(new Configuration())
+                    .withCompressionCodec(CompressionCodecName.SNAPPY)
+                    .withValidation(false)
+                    .withDictionaryEncoding(false)
+                    .build();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return  writer;
+    }
+
+    //write Arc in parquet format.
+    void parqueWriteArc(Schema schema, String outputFile) {
+        GenericRecord record = null;
+        try {
+            record = new GenericData.Record(schema);
+            File t = new File(outputFile);
+            t.delete();
+            ParquetWriter<GenericRecord> writer = parquetWriter(outputFile, schema);
+            for (String key : arcTransition.keySet()) {
+                JSONObject object = new JSONObject("{\"arc\":" + key + "}");
+                JSONArray array = object.getJSONArray("arc");
+                //System.out.println(array.get(0));
+                record.put("src", array.get(0));
+                record.put("dst", array.get(1));
+                record.put("transition", arcTransition.get(key));
+                writer.write(record);
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    //write Node in parquet format.
+    void parquetWriteNode(Schema schema, String outputFile) {
+        Schema listTokenSchema[] = new Schema[(int) P]; // schema of listTokenace = list of Token
+        Schema tokenSchema[] = new Schema[(int) P]; // schema of token in listTokenace i-th.
+        GenericRecord petriNet;
+        GenericArray listToken[] = new GenericData.Array[(int) P];
+        GenericRecord token[] = new GenericRecord[(int) P];
+        List<List<String>> colSet = new ArrayList<>();
+        List<String> oneSet;
+        try {
+            for (int i = 0; i < P; ++i) {
+                listTokenSchema[i] = schema.getField("P" + i).schema();
+                tokenSchema[i] = listTokenSchema[i].getElementType();
+                oneSet = new ArrayList<>();
+                for (Schema.Field color : tokenSchema[i].getFields()) {
+                    oneSet.add(color.name());
+                }
+                colSet.add(oneSet);
+            }
+            petriNet = new GenericData.Record(schema);
+            //System.out.println(schema);
+            File t = new File(outputFile);
+            t.delete();
+
+            ParquetWriter<GenericRecord> writer = parquetWriter(outputFile, schema);
+
+            String content = new String();
+            for (int key : node.keySet()) {
+                for (int k = 0; k < P; ++k) {
+                    int m = node.get(key).get(k).size();
+                    if (getType(colSet.get(k).get(0)).equals("unit")) {
+                        listToken[k] = new GenericData.Array(1, listTokenSchema[k]);
+                        token[k] = new GenericData.Record(tokenSchema[k]);
+                        token[k].put("unit_0", m);
+                        listToken[k].add(0, token[k]);
+                    } else {
+                        content = Arrays.toString(node.get(key).get(k).toArray());
+                        if (content.equals("[[]]"))
+                            continue;
+                        JSONObject object = new JSONObject("{\"listToken\":" + content + "}");
+                        JSONArray array = object.getJSONArray("listToken");
+                        int h = 0;
+                        listToken[k] = new GenericData.Array(m, listTokenSchema[k]);
+                        for (Object element : array) {
+
+                            if (element.equals("[[]]"))
+                                continue;
+                            JSONObject subObj = new JSONObject("{\"Token\":" + element + "}");
+                            JSONArray subArr = subObj.getJSONArray("Token");
+                            token[k] = new GenericData.Record(tokenSchema[k]);
+
+                            int n = colSet.get(k).size();
+                            for (int i = 0; i < n; ++i) {
+                                Object value = getValue(subArr.get(i).toString(), colSet.get(k).get(i));
+                                token[k].put(colSet.get(k).get(i), value);
+                            }
+                            listToken[k].add(h, token[k]);
+                            h++;
+                        }
+                    }
+                    petriNet.put("id", key);
+                    petriNet.put("P" + k, listToken[k]);
+                }
+
+                //  System.out.println(petriNet);
+                writer.write(petriNet);
+            }
+
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
