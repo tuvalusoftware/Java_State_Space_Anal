7961d846f7d02aa9fdeb045ad614810a9bc4ea44
The-Thong Nguyen <nguyenthethong1996@gmail.com>
2019-02-21 11:09:55 +0700

    add unit tests

--- src/test/java/io/ferdon/statespace/generateVarMappingTest03.java
+++ src/test/java/io/ferdon/statespace/generateVarMappingTest03.java
@@ -8,16 +8,16 @@ import java.util.Map;
 
 import static io.ferdon.statespace.main.parseJson;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 
-public class generateVarMappingTest01 {
+public class generateVarMappingTest03 {
+
     private PetrinetModel model;
     private Petrinet net;
     private Place place00, place01, place02, place03, place04, place05, place06, place07;
 
     @Before
     public void setUp() {
-        String relativePath = "/src/test/java/io/ferdon/statespace/PetrinetJson/petrinet02.json";
+        String relativePath = "/src/test/java/io/ferdon/statespace/PetrinetJson/petrinet04.json";
         String filename = System.getProperty("user.dir") + relativePath;
         model = parseJson(filename);
         net = new Petrinet(model);
@@ -51,86 +51,52 @@ public class generateVarMappingTest01 {
     @Test
     public void testVarMappingPlace2() {
         Map<String, List<String>> vars02 = place02.getVarMapping();
-        assertEquals(2, vars02.size());
-        assertEquals(1, vars02.get("r").size());
-        assertEquals(1, vars02.get("z").size());
-        assertEquals("a b +", vars02.get("r").get(0));
-        assertEquals("a b +", vars02.get("z").get(0));
+        assertEquals(1, vars02.size());
+        assertEquals(1, vars02.get("c").size());
+        assertEquals("c", vars02.get("c").get(0));
     }
 
     @Test
     public void testVarMappingPlace3() {
         Map<String, List<String>> vars03 = place03.getVarMapping();
         assertEquals(1, vars03.size());
-        assertEquals(1, vars03.get("c").size());
-        assertEquals("c", vars03.get("c").get(0));
-        assertFalse(vars03.containsKey("a"));
+        assertEquals(1, vars03.get("d").size());
+        assertEquals("d", vars03.get("d").get(0));
     }
 
     @Test
     public void testVarMappingPlace4() {
         Map<String, List<String>> vars04 = place04.getVarMapping();
-        assertEquals(2, vars04.size());
-        assertEquals(2, vars04.get("e").size());
-        assertEquals(2, vars04.get("f").size());
-        assertEquals("a b -", vars04.get("e").get(0));
-        assertEquals("c 1 +", vars04.get("e").get(1));
-        assertEquals("a b +", vars04.get("f").get(0));
-        assertEquals("c 1 -", vars04.get("f").get(1));
+        assertEquals(1, vars04.size());
+        assertEquals(1, vars04.get("e").size());
+        assertEquals("e", vars04.get("e").get(0));
     }
 
     @Test
     public void testVarMappingPlace5() {
         Map<String, List<String>> vars05 = place05.getVarMapping();
-        assertEquals(2, vars05.size());
-        assertEquals(1, vars05.get("e").size());
-        assertEquals(1, vars05.get("d").size());
-        assertEquals("e", vars05.get("e").get(0));
-        assertEquals("d", vars05.get("d").get(0));
-        assertFalse(vars05.containsKey("a"));
+        assertEquals(1, vars05.size());
+        assertEquals(1, vars05.get("f").size());
+        assertEquals("f", vars05.get("f").get(0));
     }
 
     @Test
     public void testVarMappingPlace6() {
         Map<String, List<String>> vars06 = place06.getVarMapping();
-        assertEquals(2, vars06.size());
-        assertEquals(1, vars06.get("z").size());
-        assertEquals("a b +", vars06.get("z").get(0));
-        assertEquals(1, vars06.get("r").size());
-        assertEquals("a b +", vars06.get("r").get(0));
+        assertEquals(1, vars06.size());
+        assertEquals(3, vars06.get("x").size());
+        assertEquals("a b +", vars06.get("x").get(0));
+        assertEquals("c d -", vars06.get("x").get(1));
+        assertEquals("e f *", vars06.get("x").get(2));
     }
 
     @Test
     public void testVarMappingPlace7() {
         Map<String, List<String>> vars07 = place07.getVarMapping();
-        assertEquals(3, vars07.size());
-        assertEquals(3, vars07.get("e").size());
-        assertEquals(1, vars07.get("d").size());
-        assertEquals(2, vars07.get("f").size());
-        assertEquals("a b -", vars07.get("e").get(0));
-        assertEquals("c 1 +", vars07.get("e").get(1));
-        assertEquals("e", vars07.get("e").get(2));
-        assertEquals("d", vars07.get("d").get(0));
-        assertEquals("a b +", vars07.get("f").get(0));
-        assertEquals("c 1 -", vars07.get("f").get(1));
-        assertFalse(vars07.containsKey("a"));
+        assertEquals(1, vars07.size());
+        assertEquals(3, vars07.get("x").size());
+        assertEquals("a b +", vars07.get("x").get(0));
+        assertEquals("c d -", vars07.get("x").get(1));
+        assertEquals("e f *", vars07.get("x").get(2));
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
