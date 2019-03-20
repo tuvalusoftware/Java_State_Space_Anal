8a72fce3b6ca5985a601e0bf7533bef13e0a6c53
Dang Duy Nam <dangduynam95@gmail.com>
2019-03-20 15:23:29 +0700

    init commit

---
+++ src/main/java/io/ferdon/solver/Inequalities.java
@@ -0,0 +1,16 @@
+package io.ferdon.solver;
+
+public class Inequalities {
+
+    private String[] vars;
+    private String[] constraints;
+
+    public String[] getVars() {
+        return vars;
+    }
+
+    public String[] getConstraints() {
+        return constraints;
+    }
+
+}
