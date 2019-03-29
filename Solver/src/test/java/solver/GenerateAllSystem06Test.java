4d89628852d4ec640c55c53d6c4732bbf6503bd9
The-Thong Nguyen <nguyenthethong1996@gmail.com>
2019-03-29 17:58:29 +0700

    add complex guard 02 test

--- Solver/src/test/java/solver/GenerateAllSystem06Test.java
+++ Solver/src/test/java/solver/GenerateAllSystem06Test.java
@@ -8,7 +8,7 @@ import java.util.*;
 import static org.junit.Assert.assertEquals;
 import static solver.main.parseJson;
 
-public class GenerateAllSystem05Test {
+public class GenerateAllSystem06Test {
 
     private PetrinetModel model;
     private Petrinet net;
@@ -18,29 +18,27 @@ public class GenerateAllSystem05Test {
 
     @Before
     public void setUp() {
-        String relativePath = "/src/main/java/PetrinetJson/complexGuard01.json";
+        String relativePath = "/src/main/java/PetrinetJson/complexGuard02.json";
         String filename = System.getProperty("user.dir") + relativePath;
         model = parseJson(filename);
         net = new Petrinet(model);
         place00 = net.getPlace(0);
         place01 = net.getPlace(1);
         place02 = net.getPlace(2);
-        place03 = net.getPlace(3);
         transition00 = net.getTransition(0);
         interpreter = new Interpreter();
     }
 
     @Test
     public void testGenerateAllSystem() {
-        List<LinearSystem> listSystem = net.generateListCompleteSystems(place03);
+        List<LinearSystem> listSystem = net.generateListCompleteSystems(place02);
         assertEquals(1, listSystem.size());
 
         Iterator it = listSystem.get(0).getInequalities().iterator();
         assertEquals("", it.next());
-        assertEquals("a b + 2 a b - * - 3 * 3 a b - - + 0 >", it.next());
-        assertEquals("a b + 5 * 10 a b - * - 2 * 3 - a b + 5 * 10 a b - * - 3 - - 0 >", it.next());
+        assertEquals("5 a -1 * * 1 3 - -10 a * -1 * -1 * * - 4 * > 0", it.next());
 
-                Set<Place> inputPlaces = new HashSet<>();
+        Set<Place> inputPlaces = new HashSet<>();
         Collections.addAll(inputPlaces, place00);
         assertEquals(inputPlaces, listSystem.get(0).getInputPlaces());
     }
